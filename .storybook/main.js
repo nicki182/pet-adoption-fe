13e135227377fcd955e88fa173016f59eec96230
nicki <nicky.stru@gmail.com>
2022-04-25 00:51:23 +0300

    storybook working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,21 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-styled-component-theme/dist/preset',
   ],
   framework: '@storybook/react',
+  webpackFinal: (config) => {
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      })
+    );
+    return config;
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,21 @@
+import { addDecorator } from '@storybook/react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import theme from '../theme/index';
+const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family:Title
+  src: url('https://fonts.googleapis.com/css2?family=Akshar&display=swap');
+}
+@font-face {
+  font-family:Text
+  src: url('https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@300&display=swap');
+}`;
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+  </>
+));
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
